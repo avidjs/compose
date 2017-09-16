3453651c0dc02a6fc683438c1e2d216d3bf792b6
Connor Wiseman <wiseman.connor@gmail.com>
2017-09-16 08:48:12 -0700

    Benchmarks -> koa-compose

--- benchmark/index.bench.js
+++ benchmark/index.bench.js
@@ -4,32 +4,56 @@
 'use strict';
 
 
-const compose = require('../lib/index.js');
-
-
-suite('@avidjs/compose', () => {
-  set('iterations', 10000);
-  set('type', 'adaptive');
-  set('mintime', 1000);
-  set('delay', 100);
-
+[ {
+  name: '@avidjs/compose',
+  fn:   require('../lib/index.js')
+}, {
+  name: 'koa-compose',
+  fn:   require('koa-compose')
+}].forEach((o) => {
   const logic = () => Promise.resolve(true);
 
   const fn = (ctx, next) => {
     return logic().then(next).then(logic);
   };
 
-  for (let exp = 0; exp <= 10; exp++) {
-    const count = Math.pow(2, exp);
-    const arr = [];
-    for (let i = 0; i < count; i++) {
-      arr.push(fn);
+  suite(`${o.name} - Creating stack`, () => {
+    set('iterations', 10000);
+    set('type', 'adaptive');
+    set('mintime', 1000);
+    set('delay', 100);
+
+    for (let exp = 0; exp <= 10; exp++) {
+      const count = Math.pow(2, exp);
+      const arr = [];
+      for (let i = 0; i < count; i++) {
+        arr.push(fn);
+      }
+
+      bench(`compose ${count}`, () => {
+        o.fn(arr);
+      });
     }
+  });
 
-    const stack = compose(arr);
+  suite(`${o.name} - Executing stack`, () => {
+    set('iterations', 10000);
+    set('type', 'adaptive');
+    set('mintime', 1000);
+    set('delay', 100);
 
-    bench(`(fn * ${count})`, (done) => {
-      stack({}).then(done);
-    });
-  }
+    for (let exp = 0; exp <= 10; exp++) {
+      const count = Math.pow(2, exp);
+      const arr = [];
+      for (let i = 0; i < count; i++) {
+        arr.push(fn);
+      }
+
+      const stack = o.fn(arr);
+
+      bench(`(fn * ${count})`, (done) => {
+        stack({}).then(done);
+      });
+    }
+  });
 });
